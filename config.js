5f18edd0993c8a26aee91f7eb4814f05f7367698
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2020-06-06 01:57:18 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const PORT = 6000
+const DATABASE_USERNAME = arkadipta
+
+
+DATABASE_PASSWORD = 9434438846
+module.exports = {
+    PORT,
+    url: 'mongodb+srv://arkadipta:<password>@cluster0-rbov4.mongodb.net/test'
+}
