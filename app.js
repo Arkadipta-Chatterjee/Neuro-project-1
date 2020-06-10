ceab0057ede88aba7221ce9e70f33aaa2ffb590b
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2020-06-10 17:16:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+const validator = require('email-validator');
 
 const mongoose = require('mongoose');
 
@@ -33,22 +35,71 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+const model = require('./model');
+
 
 
 app.post('/SIGN_UP', async (req, res) => {
 
     try {
         const data = await model.create(req.body);
-        if (data.veremail !== data.email) {
+        if (validator.validate(data.email)) {
             res.status(201).json({
                 status: 'Successfully signed up!Keep a watch on your registered email id and phone number for further updates!',
                 data: data
 
+
+
             })
+            let smtpTransport = nodemailer.createTransport("SMTP", {
+                service: "Gmail",
+                auth: {
+                    user: `${data.email}`,
+                    password: `${data.password}`
+
+                }
+            })
+
+            app.get('/send', function (req, res) {
+                rand = Math.floor((Math.random() * 100) + 54);
+                host = req.get('host');
+                link = "http://" + req.get('host') + "/verify?id=" + rand;
+                mailOptions = {
+                    to: req.query.to,
+                    subject: "Please confirm your Email account",
+                    html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
+                }
+                console.log(mailOptions);
+                smtpTransport.sendMail(mailOptions, function (error, response) {
+                    if (error) {
+                        console.log(error);
+                        res.end("error");
+                    } else {
+                        console.log("Message sent: " + response.message);
+                        res.end("sent");
+                    }
+                });
+            });
+
+            app.get('/verify', function (req, res) {
+                console.log(req.protocol + ":/" + req.get('host'));
+                if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
+                    console.log("Domain is matched. Information is from Authentic email");
+                    if (req.query.id == rand) {
+                        console.log("email is verified");
+                        res.end("<h1>Email " + mailOptions.to + " is been Successfully verified");
+                    } else {
+                        console.log("email is not verified");
+                        res.end("<h1>Bad Request</h1>");
+                    }
+                } else {
+                    res.end("<h1>Request is from unknown source");
+                }
+            });
         } else {
             res.status(400).json({
                 status: 'failed',
-                message: 'Verification email and entered email does not match'
+                message: 'Email id is not valid'
             })
         }
 
@@ -63,7 +114,7 @@ app.post('/SIGN_UP', async (req, res) => {
     //     let name = req.body.name;
     //     let password = req.body.password;
     //     let email = req.body.email;
-    //     let veremail = req.body.veremail;
+
     //     let phone = req.body.phone;
 
     //     let data = {
